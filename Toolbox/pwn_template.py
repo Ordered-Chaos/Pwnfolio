68f0743321957b2ec57b180640312e80fe08fdd2
Ordered-Chaos <56853119+Ordered-Chaos@users.noreply.github.com>
2020-08-25 11:30:32 -0400

    Update pwn_template.py

--- Toolbox/pwn_template.py
+++ Toolbox/pwn_template.py
@@ -9,14 +9,18 @@ from pwn import *
 
 os ='linux'
 arch ='i386'    
-r_host = '2019shell1.picoctf.com'
-r_dir = '/problem/some_directory'
-r_user = 'User'
-r_passwd = 'XXXXXX'
-#r_port = ''                                            
-exe = 'binary_name'
+# r_host = '2019shell1.picoctf.com'
+# r_dir = '/problem/some_directory'
+# r_user = 'User'
+# r_passwd = 'XXXXXX'
+# r_port = ''                                            
+exe = './binary_name'
 argv = sys.argv
 gdbscript = 'b* main'
+s = ''
+io = ''
+begin = ''
+end = ''
 
                   # Context variables # 
 
@@ -31,36 +35,43 @@ def attach_gdb():
         gdb.attach(io, gdbscript=gdbscript)
 
 def start(argv=[], *a, **kw):
-    if args.gdb:
-        global elf = context.binary = ELF(exe)
-        global io = process(elf.path)
-        global context.log_level = 'debug'
-        attach_gdb()
-        log.info('Setting log level to %s', str(context.log_level))
-        log.info('Attaching gdb to running process')
-        log.info('gdb script = %s', str(gdbscript))
-        log.info('Starting debug exploit...')
+    global begin
+    global exe
+    global io
+    global s
+    begin = time.time()
+    if gdb:
+      io = process(elf.path)
+      context.update(log_level = 'debug')
+      attach_gdb()
+      log.info('Setting log level to %s', str(context.log_level))
+      log.info('Attaching gdb to running process')
+      log.info('gdb script = %s', str(gdbscript))
+      log.info('Starting debug exploit...')
 
-    elif args.remote:
-        global context.log_level = 'error'
-        global s = ssh(host=r_host, user=r_user, password=r_passwd)
-        global elf = exe
-        global io = s.process(elf, cwd=r_dir)
-        log.info('Remote variables set:')
-        log.info('Remote host = ', str(r_host))
-        log.info('Remote user = ', str(r_user))
-        log.info('Changing working directory to: ', str(r_dir))
-        log.info('Starting remote exploit...')
+    elif remote:
+      context.update(log_level = 'error')
+      s = ssh(host=r_host, user=r_user, password=r_passwd)
+      io = s.process(elf, cwd=r_dir)
+      log.info('Remote variables set:')
+      log.info('Remote host = ', str(r_host))
+      log.info('Remote user = ', str(r_user))
+      log.info('Changing working directory to: ', str(r_dir))
+      log.info('Starting remote exploit...')
 
     else:
-        global elf = context.binary = ELF(exe)
-        global io = process(elf.path)
-        log.info('Starting local exploit ...')
+      io = process(elf.path)
+      log.info('Starting local exploit ...')
+
+def finish():
+  global end
+  global begin
+  end = time.time()
+  print('Time elapsed: ', end - begin)
 
 #===========================================================
 #                    EXPLOIT GOES HERE
 #===========================================================
-
 start()
 
 # shellcode = asm(shellcraft.sh())
@@ -72,8 +83,9 @@ start()
 # flag = io.recv(...)
 # log.success(flag)
 
-io.interactive()
+# io.interactive()
 
+finish()
 #===========================================================
 #                       
 #===========================================================
